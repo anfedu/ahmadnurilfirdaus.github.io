1c33bf6c048fce9adff1c43c9713d77e482d497a
Ahmad Nuril Firdaus <firdausnurilahmad@gmail.com>
2021-01-05 16:54:34 +0700

    add _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,40 @@
 import React from "react";
-import "../styles/globals.less";
+import PropTypes from "prop-types";
 import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <Head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
-        <link rel="shortcut icon" href="/profileCrop.png" />
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </React.Fragment>
   );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
