6a9d03f29d13825924e2c30d119f88857bb636c9
ahmad-nuril-firdaus <62326968+ahmad-nuril-firdaus@users.noreply.github.com>
2021-01-11 23:50:36 +0000

    Deploying to gh-pages from  @ 39368f317ed9604f54b38abf35b1290a7b882aca 🚀

---
+++ _next/static/chunks/pages/contact-c4bbdced2109f5ab0c23.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[13],{ALdH:function(n,t,o){"use strict";o.r(t),o.d(t,"default",(function(){return c}));var u=o("nKUr"),_=o("5Yp1");function c(){return Object(u.jsx)(_.a,{})}},lqnA:function(n,t,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/contact",function(){return o("ALdH")}])}},[["lqnA",0,2,1,4,3,5,6]]]);
